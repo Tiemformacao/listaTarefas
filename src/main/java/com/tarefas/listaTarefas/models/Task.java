a0bba664247f510fe69c02179f3609e4e28d6b7d
Tiemformacao <tiemformacao@outlook.com>
2023-11-01 10:10:11 -0300

    Fist commit

--- src/main/java/com/tarefas/listaTarefas/models/Task.java
+++ src/main/java/com/tarefas/listaTarefas/models/Task.java
@@ -2,46 +2,47 @@ package com.tarefas.listaTarefas.models;
 
 import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
 
 public class Task {
 
-	
-	private Long id;
-	private String name;
-	private Date date;
-	
-	//Criação de um construtor;
-	public Task(Long id, String name, Date date) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.date = date;
-	}
-	
-	//Criação de um construtor vazio;
-	public Task() {
-		
-	}
-	
-	public Long getId() {
-		return id;
-	}
-	
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public Date getDate() {
-		return date;
-	}
-	public void setDate(Date date) {
-		this.date = date;
-	}
-	
-	
+  public Task(Long id, String name, Date date) {
+    this.id = id;
+    this.name = name;
+    this.date = date;
+  }
+
+  public Task() {
+  }
+
+  private Long id;
+  private String name;
+
+  @DateTimeFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+  private Date date;
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setDate(Date date) {
+    this.date = date;
+  }
+
+  public Date getDate() {
+    return date;
+  }
+
 }
